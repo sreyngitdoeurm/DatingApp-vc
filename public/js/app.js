537b75d9de7f0b63dfe6a8228cd6d58f440907b9
sreyngitdoeurm <sreyngit.doeurm@student.passerellesnumeriques.org>
2021-05-21 09:33:11 +0700

    creat sign-in button

--- public/js/app.js
+++ public/js/app.js
@@ -4,25 +4,28 @@
 let ip="192.168.2.11";
 let PORT=5000;
 const getLoginRequest = "http://" + ip + ":" + PORT + "/login";
+function afterRequest(response){
+    
+}
 
 function requestLogin(event){
     event.preventDefault();
 
-    let url = getLoginRequest + "?username=" + username.value  + "&password=" + password.value + "&emailOrphonenumber" + email.value;
+    let url = getLoginRequest + "?username=" + username.value  + "&password=" + password.value + "&emailOrphonenumber=" + email.value;
 
 
     axios
         .get(url)
         .then((response) =>{
             let loginUser = response.data;
-            let textMessage = "Login Fail";
-            let color = "red";
-            if (loginUser){
-                textMessage = "Login success";
-                color = "green";
+            if (!(loginUser)){
+                let textMessage = "Login fail";
+                let color = "red";
+                message.textContent = textMessage;
+                message.style.color = color;
             }
-            message.textContent = textMessage;
-            message.style.color = color;
+            
+           
 
         });
         
@@ -51,4 +54,5 @@ const bntLogin = document.querySelector("#btn-login");
 
 
 bntLogin.addEventListener("click", requestLogin);
+console.log(email.value);
 
