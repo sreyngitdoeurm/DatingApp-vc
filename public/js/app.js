7b704d4731850c175068d5191711e493f9b816e0
sreyngitdoeurm <sreyngit.doeurm@student.passerellesnumeriques.org>
2021-05-24 17:01:46 +0700

    create users account

--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,17 @@
 
-
-
-let ip="192.168.2.11";
+let ip="localhost";
 let PORT=5000;
 const getLoginRequest = "http://" + ip + ":" + PORT + "/login";
-function afterRequest(response){
-    
-}
+const getSigningRequest = "http://" + ip + ":" + PORT + "/signin";
+let formInfo=document.querySelector('.form-info');
+formInfo.style.display='none';
+
+// ------------------------------------------------------login form------------------------------------------------------------------------
 
 function requestLogin(event){
     event.preventDefault();
-
-    let url = getLoginRequest + "?username=" + username.value  + "&password=" + password.value + "&emailOrphonenumber=" + email.value;
-
-
+    
+    let url = getLoginRequest + "?username=" + username.value  + "&password=" + password.value + "&emai=" + email.value;
     axios
         .get(url)
         .then((response) =>{
@@ -23,17 +21,38 @@ function requestLogin(event){
                 let color = "red";
                 message.textContent = textMessage;
                 message.style.color = color;
+            }else{
+                let formLogin=document.querySelector('.form-login');
+                formLogin.style.display='none';
+                formInfo.style.display='block';
+                formInfo.style.display='flex';
             }
-            
-           
-
         });
-        
-
     
-
+}
+// ------------------------------------------------------form sign in---------------------------------------------
+function singIn(event){
+    event.preventDefault();
+    let url = getSigningRequest + "?username=" + username.value  + "&password=" + password.value + "&email=" + email.value;
+    axios
+        .get(url)
+        .then((response) =>{
+            let signinUser = response.data;
+            let textMessage = "this account already sign in";
+            let color = "red";
+            if (signinUser){
+                textMessage = 'Your sign in is succesful';
+                color = "green";
+                
+            }
+            message.textContent = textMessage;
+            message.style.color = color;
+            
+            
+        });
 }
 
+// ------------------------------------------------------form information---------------------------------------------
 
 
 
@@ -51,8 +70,9 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const message=document.querySelector(".text");
 const bntLogin = document.querySelector("#btn-login");
-
+const btnSignin=document.querySelector('#btn-signin');
 
 bntLogin.addEventListener("click", requestLogin);
-console.log(email.value);
+btnSignin.addEventListener('click', singIn);
+
 
--- server.js
+++ server.js
@@ -1,28 +1,46 @@
+const fs=require('fs');
 const express = require('express');
+
 const app = express();
 
 app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
-users=[
-    {name:"Srey Ngit Doeurm", password:123456, emailOrphoneNumber:"sreyngit.doeurm@email.com"},
-    {name:"Sophea Suon", password:1234567, emailOrphoneNumber:"068875198"},
-    {name:"Chorn Sophana", password:1234568, emailOrphoneNumber:"097347407"},
-    {name:"Mang Nimuot", password:1234569, emailOrphoneNumber:"068874089"},
-    {name:"Nguon Somphors", password:234561, emailOrphoneNumber:"068913467"}
-
-]
-
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
+
 app.get('/', (req, res) => res.send("Hello Project"))
+let array=[];
+app.get('/signin', (req,res) =>{
+    let username=req.query.username;
+    let password=req.query.password;
+    let email=req.query.email;
+    let notSign=true;
+    
+    let odj={};    
+    for (let user of users){
+        if (user.name===username && user.password===password && user.email===email){
+            notSign=false;
+        }else{
+            odj.username=username;
+            odj.password=password;
+            odj.email=email;
+            array.push(odj);
+        }
+        
+    }
+    res.send(notSign);
+    // fs.writeFileSync('userAcc.json', JSON.stringify(array));
+
+    console.log(array);
+})
 app.get('/login', (req, res)=>{
     let username=req.query.username;
     let password=req.query.password;
-    let emailOrphonenumber=req.query.emailOrphonenumber;
+    let email=req.query.email;
     let loginUser=false;
     for (let user of users){
-        if (user.name===username && String(user.password)===password && user.emailOrphoneNumber===String(emailOrphonenumber)){
+        if (user.name===username && user.password===password && user.email===email){
             loginUser=true;
         }
     }
@@ -30,3 +48,10 @@ app.get('/login', (req, res)=>{
     
     
 })
+
+
+
+
+let users=JSON.parse(fs.readFileSync('userAcc.json'))
+
+
